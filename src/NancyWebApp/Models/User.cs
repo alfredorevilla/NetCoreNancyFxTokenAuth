27facb8959dee48f79ca23eca499e3c24257263e
Alfredo Revilla <alfredorevilla@gmail.com>
2016-12-22 15:01:37 -0500

    1st nancy module and enpoint ready and working.

---
+++ src/NancyWebApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NancyWebApp.Models
+{
+    public class User
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+    }
+}
---
+++ src/NancyWebApp/UserModule.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+using NancyWebApp.Models;
+using System.Collections.Generic;
+
+namespace NancyWebApp
+{
+    public class UserModule : NancyModule
+    {
+        public UserModule()
+        {
+            this.Get<IEnumerable<User>>("/nancy/users/", o => new[] { new User { Id = "arevilla", UserName = "arevilla" } }, null, "GetUsers");
+        }
+    }
+}
